b03c1ab79273294fe7f2d5d17aaf49715fa895da
Oana Berentan <oana.berentan@yahoo.com>
2019-05-12 21:40:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -51,9 +51,11 @@ def monopoly_with_fourth_tier
 			:names => {
 				:reading_railroad => {},
 				:pennsylvania_railroad => {
-					"mortgage_value" => "$100"},
+					"mortgage_value" => "$100"
+				},
 				:b_and_o_railroad => {
-					"mortgage_value" => "$200"},
+					"mortgage_value" => "$200"
+				},
 				:shortline => {}
 			},
 			:rent_in_dollars => {
